b931fe2fc06a706fe53cb9b0df1ece14d9bdab5c
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2024-09-29 20:13:16 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next"
+import "./globals.css"
+import { Ubuntu, IBM_Plex_Mono } from "next/font/google"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+  weight: ["300", "400", "500"],
+})
+const IBMPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  variable: "--font-IBM-plex-mono",
+  weight: ["400", "500"],
+})
+
+export const metadata: Metadata = {
+  title: "Web-portfolio",
+  description: "Web-portfolio",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      className={`${ubuntu.className} ${IBMPlexMono.className} bg-black-light`}
+      lang="en"
+    >
+      <body className="container">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: "390px",
+      md: "768px",
+      xl: "1280px",
+      "2xl": "1920px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "24px",
+        sm: "24px",
+        md: "64px",
+        xl: "128px",
+        "2xl": "128px",
+      },
+    },
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "var(--black)",
+          light: "var(--black-light)",
+        },
+        jade: "var(--jade)",
+        ice: "var(--ice)",
+        grey: "var(--grey)",
+        white: "var(--white)",
+        red: "var(--red)",
+        blue: {
+          DEFAULT: "var(--blue)",
+          dark: "var(--blue-dark)",
+        },
+      },
+      fontFamily: {
+        ubuntu: ["var(--font-ubuntu)"],
+        IBMPlexMono: ["var(--font-IBM-plex-mono)"],
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
